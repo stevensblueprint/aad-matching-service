53740c0a60e0ea2d376f25ed0cbafc4e142c1a22
Christian Apostol <christiand.apostol@outlook.com>
2024-11-15 13:36:08 -0500

    setup venv + requirements.txt

---
+++ data/preprocessing.py
@@ -0,0 +1,9 @@
+"""
+This module will process the raw form response data into
+a format that can be used in the Gale-Shapley algorithm.
+Form Schema can be found here:
+https://docs.google.com/spreadsheets/d/1RpGKGtm9DIUIlmZmTEvZTqQ3wwXW3MMJ/edit?gid=855474137#gid=855474137
+"""
+
+import pandas as pd
+import numpy as np
